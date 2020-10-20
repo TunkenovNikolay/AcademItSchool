552921207a5536394b5fbd870e79b5a0cf51fee8
TunkenovNikolay <nik0sh@bk.ru>
2020-10-20 15:27:06 +0700

    Кое что еще подправил.

--- Shape/src/ru/academits/tunkenov/shapes/TEST.java
+++ Shape/src/ru/academits/tunkenov/shapes/TEST.java
@@ -2,18 +2,14 @@ package ru.academits.tunkenov.shapes;
 
 public class TEST {
     public static void main(String[] args) {
-        Shape[] shapesArray = new Shape[]{new Rectangle(10, 12), new Rectangle(4, 6), new Square(8)};
-
-        Rectangle rectangle1 = new Rectangle(10, 12);
-        System.out.println(rectangle1.getArea());
-
-        Rectangle rectangle2 = new Rectangle(4, 6);
-        System.out.println(rectangle2.getArea());
-
-        Square square1 = new Square(8);
-        System.out.println(square1.getArea());
+        Shape[] shapesArray = new Shape[]{new Rectangle(10, 12), new Rectangle(4, 6), new Square(8),
+                new Rectangle(3, 6), new Circle(6), new Triangle(2, 4, 5, 5, 2, 8)};
 
         FindingShape.ShapeMaxArea(shapesArray);
         FindingShape.ShapeSecondMaxPerimeter(shapesArray);
+
+        Square square2 = new Square(10);
+        Square square3 = new Square(12);
+        System.out.println(square2.hashCode() + " | " + square3.hashCode());
     }
 }

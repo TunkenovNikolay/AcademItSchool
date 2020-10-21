55c7be1788a4ed7ffa7afbeebc74a50a802283d3
TunkenovNikolay <nik0sh@bk.ru>
2020-10-21 19:01:51 +0700

    Сделал 1-3 часть задачи Вектор, но кажется, что совсем не правильно ^_^

---
+++ Vector/src/ru/academits/tunkenov/vector/TEST.java
@@ -0,0 +1,13 @@
+package ru.academits.tunkenov.vector;
+
+import java.util.Arrays;
+
+public class TEST {
+    public static void main(String[] args) {
+        double[] array = {30.1, 5.2, 5.6};
+        Vector vector1 = new Vector(6);
+        System.out.println(Arrays.toString(new Vector[]{vector1}));
+        //int[] array = new int[7];
+        //System.out.println(Arrays.toString(array));
+    }
+}
---
+++ Vector/src/ru/academits/tunkenov/vector/Vector.java
@@ -0,0 +1,52 @@
+package ru.academits.tunkenov.vector;
+
+import java.util.Arrays;
+
+public class Vector {
+    private double[] array;
+
+    public Vector(int n) {
+        this.array = new double[n];
+    }
+
+    public Vector(Vector other) {
+        this(other.array);
+    }
+
+    public Vector(double[] other) {
+        double[] array = new double[other.length];
+
+        for (int i = 0; i <= other.length - 1; i++) {
+            array[i] = other[i];
+        }
+
+        this.array = array;
+    }
+
+    public Vector(int n, double[] other) {
+        double[] array = new double[n];
+
+        for (int i = 0; i <= other.length - 1; i++) {
+            array[i] = other[i];
+        }
+
+        this.array = array;
+    }
+
+    public double[] getArray() {
+        return array;
+    }
+
+    public void setArray(double[] array) {
+        this.array = array;
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" + "array=" + Arrays.toString(array) + '}';
+    }
+}
